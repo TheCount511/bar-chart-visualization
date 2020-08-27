3453f48f7bffcfbf7ec6e34e9e8c81ee01506fbb
Fadhil <fadhil.akins@gmail.com>
2020-08-27 17:28:14 +0100

    updates

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 
 const url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json';
 
-const canvasWidth = 1230;
-const canvasHeight = 830;
+const canvasWidth =800;
+const canvasHeight = 400;
+const barWidth = canvasWidth/275;
+const padding = 30;
 
 
 const formatYears = (dateInput)=>{
@@ -45,58 +47,57 @@ const fetchJson=( url)=>{
 		const fullDates = dates.map(item=>{
 			return new Date(item)
 		})
+		const chartArray = values.map((item,index)=>
+		{
+ 		 return [ fullDates[index], item, formatTooltip(dates[index], item)]
+			})
 
-const chartArray = values.map((item,index)=>{
- return [ fullDates[index], item, formatTooltip(dates[index], item)]
-})
 
+			return chartArray
+			})
 
-return chartArray
+.then(value => {
 
-}).then(value => {
+const gpdMax = d3.max(value, (d)=> d[1]);
+const minDate = d3.min(value, (d)=> d[0]);
+const maxDate = d3.max(value, (d)=> d[0]);
 
-/*const tooltip = d3.select("body")
-			.append("div") 
-    		.attr("class", "tooltip")               
-    		.style("display", "block");
-    .on("mouseover", function(d) {
-      tooltip.html("d[2]")
-     .style('top', d3.event.canvasHeight - 12 + 'px')
-     .style('left', d3.event.canvasWidth + 25 + 'px')
-     .style("display", "block")
-      })
-	.on("mouseout", function(d) {
-    tooltip.style("display", "none");
-});*/
-const padding = 30;
-const xScale =  d3.scaleLinear()
-.domain([
-	d3.min(value, (d)=> d[0]), 
-	d3.max(value, (d)=> d[0].setMonth(d[0].getMonth() + 3))
-	 ])
-.range([padding, canvasWidth-padding]);
+const xScale =  d3.scaleTime()
+.domain([minDate, maxDate])
+.range([0, canvasWidth]);
 
 const yScale = d3.scaleLinear()
-.domain([
-	0, 
-	d3.max(value, (d)=> d[1])
-	])
-.range([canvasHeight-padding, padding]);
+.domain([0, gpdMax])
+.range([0, canvasHeight]);
+
+const xAxis = d3.axisBottom(xScale);
+const yAxis = d3.axisLeft(yScale);
 
 const svg = d3.select("body")
  	.append("svg")
- 	.attr("width", canvasWidth)
- 	.attr("height", canvasHeight);
+ 	.attr("width", canvasWidth + 100)
+ 	.attr("height", canvasHeight + 60);
  	
  svg.selectAll("rect")
   .data(value)
   .enter()
   .append("rect")
   .attr("x", (d)=> xScale(d[0]))
-  .attr("y", (d)=> yScale(d[1]))
-  .attr("width", (d)=> 2)
-  .attr("height", (d) => canvasHeight- yScale(d[1]))
+  .attr("y", (d)=> canvasHeight - yScale(d[1]))
+  .attr("width", (d) => barWidth )
+  .attr("height", (d) =>   yScale(d[1]))
   .attr("class", "bar");
+  
+  svg.append("g")
+  .attr("transform", `translate(0, ${canvasHeight})`)
+  .call(xAxis)
+
+  svg.append("g")
+  .attr("transform", `translate(60, 0)`)
+  .call(yAxis);
+
+	 
+
 
  /* svg.selectAll("text")
   .data(value)
@@ -113,3 +114,17 @@ const svg = d3.select("body")
 }
 
 fetchJson(url);
+
+/*const tooltip = d3.select("body")
+			.append("div") 
+    		.attr("class", "tooltip")               
+    		.style("display", "block");
+    .on("mouseover", function(d) {
+      tooltip.html("d[2]")
+     .style('top', d3.event.canvasHeight - 12 + 'px')
+     .style('left', d3.event.canvasWidth + 25 + 'px')
+     .style("display", "block")
+      })
+	.on("mouseout", function(d) {
+    tooltip.style("display", "none");
+});*/
